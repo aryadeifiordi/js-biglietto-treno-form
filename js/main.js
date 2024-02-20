0c88448a991f688f3bc80c9586f982be3438627f
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-20 15:43:00 +0100

    data collection

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('ciao');
+// numero di kilometri 
+// età del passeggero
+
+// prezzo 0.21 euro al km 
+// sconto 20% minorenni 
+// sconto 40% over 65 
+
+// verificare età
+
+
+const TravelKm = Number(prompt('How many km have to travel?'));
+
+const Age = Number(prompt('How old are you?'));
