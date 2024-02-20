dc051dd35999badf3e9ecf5952c84afeb40456ea
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-20 16:06:08 +0100

    js milestone 2

---
+++ MILESTONE 2/asset/JS/main.js
@@ -0,0 +1,54 @@
+// numero di kilometri 
+// età del passeggero
+
+// prezzo 0.21 euro al km 
+// sconto 20% minorenni 
+// sconto 40% over 65 
+
+// verificare età
+
+// raccolta dati
+
+const TravelKm = Number(prompt('How many km have to travel?'));
+
+const Age = Number(prompt('How old are you?'));
+
+// stampa dati 
+
+console.log('km')
+console.log(typeof TravelKm);
+console.log(TravelKm);
+console.log('age')
+console.log(typeof Age);
+console.log(Age);
+
+
+// calcolo prezzo
+
+let price = 0.21, result; // prezzo standard 
+
+// se minore di 18 anni = sconto 20% su prezzo standard 
+
+// se maggiore di 65 anni = sconto 40% su prezzo standard 
+
+if(Age < 18){
+    //sconto 20%
+    discount = ((TravelKm * price) * 0.2);
+    result = ((TravelKm * price) - discount);
+
+    
+} else {
+    //standard price
+    result = TravelKm * price;
+}
+
+if(Age > 65){
+  // sconto 40%
+  discount = ((TravelKm * price) * 0.4);
+  result = ((TravelKm * price) - discount);
+
+}
+
+console.log('il prezzo del tuo biglietto: ' + result.toFixed(2)); 
+
+// se sono 17 km * 0,21 = 3.57 + sconto 
